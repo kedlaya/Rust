bbfa476fed22c9dbc516490c3297ee042ba45971
Kiran S. Kedlaya <kskedl@gmail.com>
2025-06-29 18:44:11 -0700

    Implement flexible version of the main loop

--- src/main.rs
+++ src/main.rs
@@ -3,67 +3,98 @@ mod cyclotomic;
 use cyclotomic::{CyclotomicIntegerExponents, test_cyclotomic_integer_exponents};
 use std::fs::File;
 use std::io::Write;
+use itertools::Itertools;
+
+fn loop_over_roots(n: u32, len: usize, mut f: &File) -> std::io::Result<()> {
+   let mut l: Vec<u32> = vec![0; len];
+   let n2 = if n%2 == 0 {n / 2} else {0};
+   let n3 = if (n2 != 0) && (n%3 == 0) {n / 3} else {0};
+   let n5 = if (n2 != 0) && (n%5 == 0) {n / 5} else {0};
+
+   for d in 1..n {
+       if n % d != 0 {
+           continue;
+       }
+       l[1] = d; 
+       // Loop over tuples [j_1, ..., j_*] with 0 <= j_1 <= ... <= j_* <= n/d.
+       // Note: we allow n as an exponent as a proxy for a zero summand.
+       'inner: for it in ((0..=n).step_by(d as usize)).combinations_with_replacement(len-2) {
+           if it[0] == it[len-3] {
+               println!("Checking cases with n = {}, d = {}, j_1 = {}", n, d, it[0]);
+           }
+
+           for i in 2..len {
+               l[i] = it[i-2];
+           }
+
+           // Remove some cases made redundant by complex conjugation.
+           if (l[len-1] < n) && ((l[2] + l[len-1] > n + l[1]) || ((l[2] == 0) && (l[3] > 1))) {
+               continue 'inner;
+           }
+
+           // Skip cases where two roots of unity differ by a factor of -1
+           if n2 != 0 {
+               for a in 0..len {
+                   if l[a] < n {
+                       for b in 0..a {
+                           if l[a] == l[b] + n2 {
+                               continue 'inner;
+                           }
+                       }
+                   }
+               }
+           }
+
+           // Skip cases where two roots of unity differ by a factor of zeta_3
+           if n3 != 0 {
+               for a in 0..len {
+                   if l[a] < n {
+                       for b in 0..a {
+                           if (l[a] == l[b] + n3) || (l[a] == l[b] + 2*n3) {
+                               continue 'inner;
+                           }
+                       }
+                   }
+               }
+           }
+    
+           // Skip cases where three roots of unity differ by factors of zeta_5
+           if n5 != 0 {
+               for a in 0..len {
+                   if l[a] < n {
+                       for b in 0..a {
+                           if (l[a] > l[b]) && ((l[a]-l[b]) % n5 == 0) {
+                               for c in 0..b {
+                                   if (l[b] > l[c]) && ((l[b]-l[c]) % n5 == 0) {
+                                       continue 'inner;
+                                   }
+                               }
+                           }
+                       }
+                   }
+               }
+           }
+
+           // Filter for house squared <= 5.1
+           let ex = CyclotomicIntegerExponents{ exponents: l.clone(), level: n };
+           if ex.compare_house_squared(5.1 as f64) {
+              // Record the exponents (omitting the initial 0)
+              println!("{:?}", l);
+              write!(f, "{}, {:?}\n", n, l).expect("output failure");
+          }
+      }
+   }
+   Ok(())
+}
 
 fn main() -> std::io::Result<()> {
    test_cyclotomic_integer_exponents();
-   let mut f = File::create("output.txt")?;
-   let n = 420;
-   let n2 = n / 2;
-   let n3 = n / 3;
-   let len = 7;
+   println!("All tests passed!");
 
-   for d in 1..n {
-      if n % d != 0 {
-          continue;
-      }
-      // Note: we allow n as an exponent as a proxy for a zero summand.
-      let r = (n / d) + 1;
-      // Loop over tuples [j_1, ..., j_5] with 0 <= j1 <= ... <= j5 <= n/d
-      for j1 in 0..r {
-         println!("Checking cases with d = {}, j_0 = {}", d, j1);
-         for j2 in j1..r {
-            for j3 in j2..r {
-               for j4 in j3..r {
-                  'inner: for j5 in j4..r {
-                     let l = vec![0, d, d*j1, d*j2, d*j3, d*j4, d*j5];
+   let f = File::create("output.txt")?;
+   let _ = loop_over_roots(11, 5, &f);
+   let _ = loop_over_roots(420, 7, &f);
 
-                     // Remove some cases made redundant by complex conjugation.
-                     if (l[len-1] < n) && (l[2]-l[1] > n - l[len-1]) {
-                         continue;
-                     }
-
-                     // Skip cases where two roots of unity sum to zero
-                     for a in 0..len {
-                        for b in 0..a {
-                           if (l[a] < n) && (l[a] == l[b] + n2) {
-                              break 'inner;
-                           }
-                        }
-                     }
-
-                     // Skip cases where three roots of unity sum to zero
-                     for a in 0..len {
-                        for b in 0..a {
-                           for c in 0..b {
-                              if (l[a] < n) && (l[a] == l[b] + n3) && (l[b] == l[c] + n3) {
-                                 break 'inner;
-                              }
-                           }
-                        }
-                     }
-
-                     // Filter for house squared <= 5.1
-                     let ex = CyclotomicIntegerExponents{ exponents: l.clone(), level: n };
-                     if ex.compare_house_squared(5.1 as f64) {
-                        // Record the exponents (omitting the initial 0)
-                        write!(f, "[{}, {}, {}, {}, {}, {}]\n", l[1], l[2], l[3], l[4], l[5], l[6]).expect("output failure");
-                     }
-                  }
-               }
-            }
-         }
-      }
-   }
    println!("All cases checked!");
    Ok(())
 }
