1ab80e197e5a4b47202f09b1faaa8c0c655fa05c
Kiran S. Kedlaya <kskedl@gmail.com>
2025-09-18 23:08:02 -0700

    Remove test code

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,6 @@ use std::io::Result;
 fn main() -> Result<()> {
     let file_tables = File::create("tables.txt")?;
     let file_output = File::create("output.txt")?;
-    let inputs = [(1, 1)];
-    for (n0, len) in inputs {
-        loop_over_roots(n0, len, &file_tables, &file_output);
-    }
-
     let inputs = [(19,              9),
                   (2*2*3*7,         8),
                   (2*2*3*5,         8),
