aab785eda23bc6b7b9c15a57a09840699b06bcff
Kiran S. Kedlaya <kskedl@gmail.com>
2025-09-20 03:11:11 -0700

    Run only the cases we need for Lemma 3.11

--- src/main.rs
+++ src/main.rs
@@ -9,26 +9,14 @@ use std::io::Result;
 fn main() -> Result<()> {
     let file_tables = File::create("tables.txt")?;
     let file_output = File::create("output.txt")?;
-    let inputs = [(19,              9),
-                  (2*2*3*7,         8),
-                  (2*2*3*5,         8),
-                  (7*13,            7),
-                  (2*2*3*5*7,       7),
-                  (31,              6),
-                  (29,              6),
-                  (23,              6),
-                  (2*2*13,          6),
-                  (3*5*11,          6),
-                  (3*3*7,           6),
-                  (3*3*5,           6),
-                  (3*7*11*13,       5),
-                  (3*5*7*13,        5),
-                  (2*2*3*5*7*11,    5),
-                  (5*19,            4),
-                  (5*17,            4),
-                  (2*2*3*5*7*11*13, 4),
-                  (2*2*2*3*3*5*7,   4),
-                  (2*2*2*2*3*5,     4)];
+    let inputs = [(2*2*3*5*7,       7),  // Proposition 4.3
+                  (31,              6),  // Remark 8.3
+                  (3*5*7*13,        5),  // Section 8.3.1
+                  (2*2*3*5*7*11,    5),  // Sections 4.2.1, 8.2.1
+                  (5*19,            4),  // Section 4.2.4
+                  (5*17,            4),  // Section 4.2.4
+                  (2*2*3*5*7*11*13, 4),  // Section 4.2.2
+                  (2*2*2*3*3*5*7,   4)]; // Proposition 4.1
     for (n0, len) in inputs {
         loop_over_roots(n0, len, &file_tables, &file_output);
     }
